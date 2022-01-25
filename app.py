115435133aa55474532a66ee6ffc21b44bd3510e
Lucas de Barros <lucas.bsouza2@gmail.com>
2022-01-24 22:43:16 -0300

    remove bug

--- app.py
+++ app.py
@@ -8,17 +8,7 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "Laboratório Pipeline DevOps - Lucas Barros"
-
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
+ 
 
 if __name__ == '__main__':
     app.run()
