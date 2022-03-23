4de9d8e0b322284b03fdccc70b5aefbb9b872cb5
Lucas de Barros <lucas.bsouza2@gmail.com>
2022-03-23 00:05:52 -0300

    Teste fail da pipeline

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps - Lucas de Barros"
+    return "Laboratório Pipeline DevOps"
  
 
 if __name__ == '__main__':
